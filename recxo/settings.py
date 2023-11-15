542c7ce12c91159b7f7b6edf73cc3da11d2e47ca
Recxo <rishava065@gmail.com>
2023-11-15 13:41:48 +0530

    made the DEBUG = True in settings.py

--- recxo/settings.py
+++ recxo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ez-jrk(+qsg21^*tc6lx%_=mdes)$=-gtw=l+mijpf&a2z9#2q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
