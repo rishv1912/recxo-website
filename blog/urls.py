ac0dd5abf7ec6dc7237d752406677b92adb6e523
Recxo <rishava065@gmail.com>
2024-02-13 20:32:25 +0530

    solving the error thrown 'Server Error (500)' by render

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
   path('',views.home,name='blog-home'),
   # path('profile/<str:pk>',views.userProfile,name='blog-user-profile'),
   # path('post/',views.createBlog,name='blog-create'),
-<<<<<<< HEAD
-  path('<str:pk>',views.blog,name='blog-page')
-=======
+
+  # path('<str:pk>',views.blog,name='blog-page'),
   path('<str:slug>/', views.blogPost,name='blogPost'),
->>>>>>> system
+
     
 ]
